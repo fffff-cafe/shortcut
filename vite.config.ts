0832442363f3301b3283218a82738cac8751ef29
kixixixixi <kixixixixi@gmail.com>
2024-09-29 17:20:38 +0900

    Add build copy

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { copyFileSync } from "node:fs";
+import { join } from "node:path";
 
 export default defineConfig({
   plugins: [
@@ -12,6 +14,14 @@ export default defineConfig({
       },
       ssr: false,
       basename: "/shortcut/",
+      buildEnd(args) {
+        if (!args.viteConfig.isProduction) return;
+        const buildPath = args.viteConfig.build.outDir;
+        copyFileSync(
+          join(buildPath, "index.html"),
+          join(buildPath, "404.html")
+        );
+      },
     }),
     tsconfigPaths(),
   ],
